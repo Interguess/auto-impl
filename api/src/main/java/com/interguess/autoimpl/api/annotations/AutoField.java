ce4c4ce040857a5b3550dabe821462803fe560b8
JonasDevDE <jonasschnell23@gmail.com>
2025-07-31 13:33:16 +0200

    chore: add target declaration to annotations

--- api/src/main/java/com/interguess/autoimpl/api/annotations/AutoField.java
+++ api/src/main/java/com/interguess/autoimpl/api/annotations/AutoField.java
@@ -2,11 +2,15 @@ package com.interguess.autoimpl.api.annotations;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 /**
  * Annotation to mark a method that's implementation should reference to a specific field.
  * E.g. if you have a getter and a setter you set their field name with this annotation to the same value,
  * auto-impl will generate getter and setter implementations for their common field.
  */
+@Target({ElementType.METHOD})
 public @interface AutoField {
 
     /**
--- api/src/main/java/com/interguess/autoimpl/api/annotations/AutoImpl.java
+++ api/src/main/java/com/interguess/autoimpl/api/annotations/AutoImpl.java
@@ -1,7 +1,10 @@
 package com.interguess.autoimpl.api.annotations;
 
+import java.lang.annotation.Target;
+
 /**
  * Marker annotation for classes that should be automatically implemented.
  */
+@Target({java.lang.annotation.ElementType.TYPE})
 public @interface AutoImpl {
 }
