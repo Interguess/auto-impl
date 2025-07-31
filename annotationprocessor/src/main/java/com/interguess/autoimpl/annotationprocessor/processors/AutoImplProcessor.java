f9c75e8f001bb3d7f4872786f0bf38bd3469d9c4
JonasDevDE <jonasschnell23@gmail.com>
2025-07-31 12:46:14 +0200

    chore: add AutoImplProcessor

---
+++ annotationprocessor/src/main/java/com/interguess/autoimpl/annotationprocessor/processors/AutoImplProcessor.java
@@ -0,0 +1,50 @@
+package com.interguess.autoimpl.annotationprocessor.processors;
+
+import com.google.auto.service.AutoService;
+import com.interguess.autoimpl.annotationprocessor.ImplementationClassGenerator;
+import com.interguess.autoimpl.api.annotations.AutoImpl;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.util.Set;
+
+@AutoService(Processor.class)
+@SupportedAnnotationTypes("com.interguess.autoimpl.api.annotations.AutoImpl")
+@SupportedSourceVersion(SourceVersion.RELEASE_21)
+public class AutoImplProcessor extends AbstractProcessor {
+
+    @Nullable
+    private ImplementationClassGenerator classGenerator;
+
+    @Override
+    public synchronized void init(final @NotNull ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+
+        this.classGenerator = new ImplementationClassGenerator(processingEnv);
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        if (classGenerator == null) {
+            processingEnv.getMessager().printMessage(
+                    Diagnostic.Kind.ERROR,
+                    "AutoImplProcessor is not initialized properly. " +
+                            "Please ensure that the processing environment is set up correctly."
+            );
+        }
+
+        for (final Element element : roundEnv.getElementsAnnotatedWith(AutoImpl.class)) {
+            if (element.getKind() == ElementKind.INTERFACE) {
+                classGenerator.generateForInterface((TypeElement) element);
+            }
+        }
+
+        return true;
+    }
+}
